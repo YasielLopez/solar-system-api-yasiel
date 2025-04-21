ed2ae7e342c28c149d0bb3a3ab503d1c32955c01
Yasiel <toyasiel@gmail.com>
2025-04-21 09:36:05 -0700

    committing changes

--- app/routes/planets.py
+++ app/routes/planets.py
@@ -4,12 +4,22 @@ from app.models import planet_list
 
 # creates a blueprint for planet routes
 
-planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
-    """
-    Get a list of all planets
-    """
     planets_response = [planet.to_dict() for planet in planet_list]
     return jsonify(planets_response)
+
+@planets_bp.route("<id>", methods=["GET"])
+def get_one_planet(id):
+    pass
+
+def validate_planet(id):
+    tri: id = inct()
+except value
+
+def make_responses(invalid, 400)
+return planet 
+
+

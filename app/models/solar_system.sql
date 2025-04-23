b278265a42d80754117f8eb82e240d409f4af81a
Laura Castro <lauracastrotech@gmail.com>
2025-04-23 12:11:33 -0400

    fixes migration to add a table for planet model

--- app/models/solar_system.sql
+++ app/models/solar_system.sql
@@ -6,26 +6,29 @@
 -- 1. Create a migration to add a table for the `Planet` model and then apply it. 
 --     * *Confirm that the `planet` table has been created as expected in postgres*.
 
+-- Create solar system database
 CREATE DATABASE solar_system_development;
 
-CREATE TABLE planet(
+-- Create Planet model
+CREATE TABLE planets(
     'id' INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY ,
     'name' VARCHAR(30) NOT NULL,
     'description' TEXT NOT NULL,
     'diameter_km' INT NOT NULL
     );
 
+-- Add planets data to table
 INSERT INTO planet
 VALUES(
-    (1, "Mercury", "The smallest and innermost planet in the Solar System", 4879),
-    (2, "Venus", "Second planet from the Sun with thick atmosphere", 12104),
-    (3, "Earth", "Our home planet and the only known planet with life", 12742),
-    (4, "Mars", "The Red Planet, fourth from the Sun", 6779),
-    (5, "Jupiter", "The largest planet in our Solar System", 139820),
-    (6, "Saturn", "Known for its prominent ring system", 116460),
-    (7, "Uranus", "The seventh planet from the Sun", 50724),
-    (8, "Neptune", "The eighth and farthest planet from the Sun", 49244),
-);
+        (1, "Mercury", "The smallest and innermost planet in the Solar System", 4879),
+        (2, "Venus", "Second planet from the Sun with thick atmosphere", 12104),
+        (3, "Earth", "Our home planet and the only known planet with life", 12742),
+        (4, "Mars", "The Red Planet, fourth from the Sun", 6779),
+        (5, "Jupiter", "The largest planet in our Solar System", 139820),
+        (6, "Saturn", "Known for its prominent ring system", 116460),
+        (7, "Uranus", "The seventh planet from the Sun", 50724),
+        (8, "Neptune", "The eighth and farthest planet from the Sun", 49244),
+    );
 
 -- Check to see that table was created.
-SELECT * from planet;
+SELECT * from planets;

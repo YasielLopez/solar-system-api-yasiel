70d3dc1bfce70d42080a292b526e1e2c10fd922a
Laura Castro <lauracastrotech@gmail.com>
2025-04-23 12:37:56 -0400

    fixes migration file

--- app/models/solar_system.sql
+++ app/models/solar_system.sql
@@ -11,24 +11,22 @@ CREATE DATABASE solar_system_development;
 
 -- Create Planet model
 CREATE TABLE planets(
-    'id' INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY ,
-    'name' VARCHAR(30) NOT NULL,
-    'description' TEXT NOT NULL,
-    'diameter_km' INT NOT NULL
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY ,
+    name VARCHAR(30) NOT NULL,
+    description TEXT NOT NULL,
+    diameter_km INT NOT NULL
     );
 
 -- Add planets data to table
-INSERT INTO planet
-VALUES(
-        (1, "Mercury", "The smallest and innermost planet in the Solar System", 4879),
-        (2, "Venus", "Second planet from the Sun with thick atmosphere", 12104),
-        (3, "Earth", "Our home planet and the only known planet with life", 12742),
-        (4, "Mars", "The Red Planet, fourth from the Sun", 6779),
-        (5, "Jupiter", "The largest planet in our Solar System", 139820),
-        (6, "Saturn", "Known for its prominent ring system", 116460),
-        (7, "Uranus", "The seventh planet from the Sun", 50724),
-        (8, "Neptune", "The eighth and farthest planet from the Sun", 49244),
-    );
+INSERT INTO planets (name, description, diameter_km)
+VALUES('Mercury', 'The smallest and innermost planet in the Solar System', 4879),
+        ('Venus', 'Second planet from the Sun with thick atmosphere', 12104),
+        ('Earth', 'Our home planet and the only known planet with life', 12742),
+        ('Mars', 'The Red Planet, fourth from the Sun', 6779),
+        ('Jupiter', 'The largest planet in our Solar System', 139820),
+        ('Saturn', 'Known for its prominent ring system', 116460),
+        ('Uranus', 'The seventh planet from the Sun', 50724),
+        ('Neptune', 'The eighth and farthest planet from the Sun', 49244);
 
 -- Check to see that table was created.
-SELECT * from planets;
+SELECT * FROM planets;

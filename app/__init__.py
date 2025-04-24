48e22842b53578cd75ef4052a302b450e8091234
Laura Castro <lauracastrotech@gmail.com>
2025-04-24 14:33:25 -0400

    gives import error

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from .db import db, migrate
 
 def create_app(test_config=None):
     app = Flask(__name__)
     
-    # Configure the Flask app
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
     
     # Register blueprints
     from app.routes.planets import planets_bp
--- app/models/Planet.py
+++ app/models/Planet.py
@@ -5,18 +5,26 @@
     Programmer: Yasiel Lopez
 '''
 
-class Planet:
-    def __init__(self, id, name, description, diameter_km):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.diameter_km = diameter_km
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    description: Mapped[str]
+    diameter_km: Mapped[int]
+# class Planet:
+#     def __init__(self, id, name, description, diameter_km):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.diameter_km = diameter_km
     
-    # creates a to_dict() method that converts the object to a dictionary format suitable for JSON responses
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "diameter_km": self.diameter_km
-        }
+#     # creates a to_dict() method that converts the object to a dictionary format suitable for JSON responses
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "name": self.name,
+#             "description": self.description,
+#             "diameter_km": self.diameter_km
+#         }

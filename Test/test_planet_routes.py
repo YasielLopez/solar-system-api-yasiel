8419a7fd275c6f8c9a14875bf5cbf2d2f5e06bad
Yasiel <toyasiel@gmail.com>
2025-05-07 10:09:00 -0700

    added get and post tests to original folder

--- Test/test_planet_routes.py
+++
@@ -1,8 +0,0 @@
-def test_get_planets(client):
-
-    response = client.get("/planets")
-    response_body = response.get_json()
-
-    assert response.status_code == 200
-    assert response_body == [] 
-
